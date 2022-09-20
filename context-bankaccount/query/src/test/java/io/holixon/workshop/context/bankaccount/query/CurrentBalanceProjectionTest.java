a4ddf0feeb11b7a286151253aa095c510c1b0622
Simon Zambrovski <simon.zambrovski@holisticon.de>
2022-09-20 22:12:32 +0200

    implemented JGiven projection tests

--- context-bankaccount/query/src/test/java/io/holixon/workshop/context/bankaccount/query/CurrentBalanceProjectionTest.java
+++ context-bankaccount/query/src/test/java/io/holixon/workshop/context/bankaccount/query/CurrentBalanceProjectionTest.java
@@ -5,26 +5,63 @@ import com.tngtech.jgiven.junit5.DualScenarioTest;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled
 class CurrentBalanceProjectionTest extends DualScenarioTest<CurrentBalanceGivenWhenStage, CurrentBalanceThenStage> {
 
   @Test
   void can_create_bankAccount() {
+    given()
+      .bankAccount_created("1", 100);
+
+    then()
+      .expect_currentBalance_for_account("1", 100);
   }
 
   @Test
   void withdraw_decreases_balance() {
+    given()
+      .bankAccount_created("1", 100);
 
+    when()
+      .withdraw_amount_from_account("1", 60);
+
+    then()
+      .expect_currentBalance_for_account("1", 40);
+    // TODO: leave this, later use as example to show benefits of using jgiven.
+    //    projection.on(new BankAccountCreatedEvent("1", 100));
+    //    projection.on(new MoneyWithdrawnEvent("1", 40));
+    //
+    //
+    //    assertThat(projection.query(new CurrentBalanceQuery("1"))).hasValue(new CurrentBalanceResponse("1", 60));
   }
 
   @Test
   void deposit_increases_balance() {
+    given()
+      .bankAccount_created("1", 100);
 
+    when()
+      .deposit_amount_on_account("1", 60);
+
+    then()
+      .expect_currentBalance_for_account("1", 160);
   }
 
   @Test
   void transfer_decreases_sourceAccount_and_increases_targetAccount() {
+    given()
+      .bankAccount_created("1", 100)
+      .and()
+      .bankAccount_created("2", 200)
+    ;
 
+    when()
+      .transfer_money("1", 25, "2");
+
+    then()
+      .expect_currentBalance_for_account("1", 75)
+      .and()
+      .expect_currentBalance_for_account("2", 225)
+    ;
   }
 
 }
--- context-bankaccount/query/src/test/java/io/holixon/workshop/context/bankaccount/query/MoneyTransferProjectionTest.java
+++ context-bankaccount/query/src/test/java/io/holixon/workshop/context/bankaccount/query/MoneyTransferProjectionTest.java
@@ -9,7 +9,6 @@ import io.holixon.workshop.context.bankaccount.api.query.MoneyTransfersQuery;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled
 class MoneyTransferProjectionTest extends DualScenarioTest<MoneyTransferGivenWhenStage, MoneyTransferThenStage> {
 
   public static final String ACCOUNT_A = "11111111";
@@ -19,28 +18,72 @@ class MoneyTransferProjectionTest extends DualScenarioTest<MoneyTransferGivenWhe
 
   @Test
   void initially_empty() {
+    given()
+      .no_prior_activity();
 
+    then()
+      .no_transfers_exist();
   }
 
   @Test
   void requested_transfer_can_be_found() {
+    given()
+      .no_prior_activity();
 
+    when()
+      .transfer_requested(new MoneyTransferRequestedEvent("1", ACCOUNT_A, ACCOUNT_B, 50));
+
+    then()
+      .transfer_exists_with_state_$(new MoneyTransfer("1", ACCOUNT_A, ACCOUNT_B, 50));
   }
 
 
   @Test
   void transfer_can_be_completed() {
+    given()
+      .transfer_requested(new MoneyTransferRequestedEvent("1", ACCOUNT_A, ACCOUNT_B, 50));
 
+
+    when()
+      .transfer_completed(new MoneyTransferCompletedEvent("1", ACCOUNT_A, 50));
+
+    then()
+      .transfer_exists_with_state_$(new MoneyTransfer("1", ACCOUNT_A, ACCOUNT_B, 50, true, null));
   }
 
   @Test
   void transfer_can_be_cancelled() {
+    given()
+      .transfer_requested(new MoneyTransferRequestedEvent("1", ACCOUNT_A, ACCOUNT_B, 50));
 
+    when()
+      .transfer_cancelled(new MoneyTransferCancelledEvent("1", "some error"));
+
+    then()
+      .transfer_exists_with_state_$(new MoneyTransfer("1", ACCOUNT_A, ACCOUNT_B, 50, false, "some error"));
   }
 
   @Test
   void can_query_transfers_for_account() {
+    given()
+      .transfer_requested(new MoneyTransferRequestedEvent("1", ACCOUNT_A, ACCOUNT_B, 50))
+      .and()
+      .transfer_requested(new MoneyTransferRequestedEvent("2", ACCOUNT_B, ACCOUNT_C, 10))
+      .and()
+      .transfer_completed(new MoneyTransferCompletedEvent("1", ACCOUNT_A, 50))
+      .and()
+      .transfer_completed(new MoneyTransferCompletedEvent("2", ACCOUNT_B, 10))
+    ;
 
+    when()
+      .query_transfers_for_account(new MoneyTransfersQuery(ACCOUNT_A))
+    ;
+
+    then()
+      .query_response_contains(
+        new MoneyTransfer("1", ACCOUNT_A, ACCOUNT_B, 50, true, null)
+      );
   }
+
 
 }
