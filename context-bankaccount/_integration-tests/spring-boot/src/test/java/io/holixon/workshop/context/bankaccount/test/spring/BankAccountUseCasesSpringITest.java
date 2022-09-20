7909a49d13b3ab2a0a3b308a6c06145434103e99
Simon Zambrovski <simon.zambrovski@holisticon.de>
2022-09-20 22:31:03 +0200

    implement spring integration tests

--- context-bankaccount/_integration-tests/spring-boot/src/test/java/io/holixon/workshop/context/bankaccount/test/spring/BankAccountUseCasesSpringITest.java
+++ context-bankaccount/_integration-tests/spring-boot/src/test/java/io/holixon/workshop/context/bankaccount/test/spring/BankAccountUseCasesSpringITest.java
@@ -10,32 +10,91 @@ import java.util.UUID;
 
 @SpringBootTest(classes = TestConfiguration.class)
 @EnableJGiven
-@Disabled
 public class BankAccountUseCasesSpringITest extends DualSpringScenarioTest<BankAccountActionStage, BankAccountAssertStage> {
 
   @Test
   void create_account_and_query_balance() {
+    String account1 = accountId();
 
+    given()
+      .create_bankAccount(account1, 100);
+
+    then()
+      .account_has_balance(account1, 100);
   }
 
   @Test
   void withdraw_amount() {
+    String account1 = accountId();
 
+    given()
+      .create_bankAccount(account1, 100);
+
+    when()
+      .withdraw(account1, 10);
+
+    then()
+      .account_has_balance(account1, 90);
   }
 
   @Test
   void deposit_amount() {
+    String account1 = accountId();
 
+    given()
+      .create_bankAccount(account1, 100)
+    ;
+
+    when()
+      .deposit(account1, 10)
+    ;
+
+    then()
+      .account_has_balance(account1, 110)
+    ;
   }
 
   @Test
   void transfer_10_from_source_to_target() {
+    String account1 = accountId();
+    String account2 = accountId();
 
+    given()
+      .create_bankAccount(account1, 100)
+      .and()
+      .create_bankAccount(account2, 0)
+    ;
+    when()
+      .transfer(account1, account2, 30)
+    ;
+
+    then()
+      .account_has_balance(account1, 70)
+      .and()
+      .account_has_balance(account2, 30)
+    ;
   }
 
   @Test
   void transfer_fails_when_target_would_exceed_maxBalance() {
+    String account1 = accountId();
+    String account2 = accountId();
 
+    given()
+      .create_bankAccount(account1, 100)
+      .and()
+      .create_bankAccount(account2, 990)
+    ;
+
+    when()
+      .transfer(account1, account2, 30)
+    ;
+
+    then()
+      .account_has_balance(account1, 100)
+      .and()
+      .account_has_balance(account2, 990)
+    ;
   }
 
   private String accountId() {
