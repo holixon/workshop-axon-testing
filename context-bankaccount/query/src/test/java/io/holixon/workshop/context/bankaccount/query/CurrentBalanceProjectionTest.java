e57f2a28e4bdae4be9754f74620337cb3a526347
Jan Galinski <jan.galinski@holisticon.de>
2022-09-21 22:39:22 +0200

    prepare class 2 - given/when/then projection test

--- context-bankaccount/query/src/test/java/io/holixon/workshop/context/bankaccount/query/CurrentBalanceProjectionTest.java
+++ context-bankaccount/query/src/test/java/io/holixon/workshop/context/bankaccount/query/CurrentBalanceProjectionTest.java
@@ -10,21 +10,24 @@ class CurrentBalanceProjectionTest extends DualScenarioTest<CurrentBalanceAction
 
   @Test
   void can_create_bankAccount() {
+    // GIVEN: nothing happened
+    // WHEN: bank account created with id and initial balance
+    // THEN query for current balance for id returns balance
   }
 
   @Test
   void withdraw_decreases_balance() {
-
-  }
-
-  @Test
-  void deposit_increases_balance() {
-
+    // GIVEN: account with id and balance
+    // WHEN amount is withdrawn from account
+    // THEN query for current balance for id returns balance - amount
   }
 
   @Test
   void transfer_decreases_sourceAccount_and_increases_targetAccount() {
-
+    // GIVEN two accounts with id and balance
+    // WHEN we transfer amount from 1st to 2nd
+    // THEN query for 1st account returns balance - amount
+    // AND query for 2nd account returns balance + account
   }
 
 }
